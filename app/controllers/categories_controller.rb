87ca97d9287c5106d4848155f523c87d326c608f
JuliaMartin11 <juliamartin1113@gmail.com>
2025-07-16 19:54:24 +0800

    Restrict category access to current user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,23 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_category, only: %i[ show edit update destroy ]
 
-  # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
   end
 
-  # GET /categories/new
   def new
-    @category = Category.new
+    @category = current_user.categories.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.new(category_params)
 
     respond_to do |format|
       if @category.save
@@ -34,7 +30,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
   def update
     respond_to do |format|
       if @category.update(category_params)
@@ -47,7 +42,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy!
 
@@ -58,13 +52,12 @@ class CategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_category
-      @category = Category.find(params.expect(:id))
+      @category = current_user.categories.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def category_params
-      params.expect(category: [ :name, :user_id ])
+      params.require(:category).permit(:name)
     end
 end
